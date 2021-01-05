b7f90c121d90093ec34ce04ff4671634e27e17b4
Ramos0921 <sfcc.eduardo.ramos@gmail.com>
2021-01-05 14:09:27 -0700

    server routes complete CRUD

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,17 @@
 import { getAllListingsRoute } from './getAllListings';
 import { getListingsRoute } from './getListing';
+import { addViewToListingRoute } from './addViewToListing';
+import { getUserListingsRoute } from './getUserListings';
+import { createNewListingRoute } from './createNewListing';
+import { updateListingRoute } from './updateListings';
+import { deleteListingRoute } from './deleteListing'
 
 export const routes = [
   getAllListingsRoute,
   getListingsRoute,
+  addViewToListingRoute,
+  getUserListingsRoute,
+  createNewListingRoute,
+  updateListingRoute,
+  deleteListingRoute,
 ]
